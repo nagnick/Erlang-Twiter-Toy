8b0cbeb8df3fe0a54f72ba5cf8d0cb6c47a633c4
nicolas <nicolasgarcia@ufl.edu>
2022-11-25 21:40:22 -0500

    Added basic user following/follower logic

--- src/twitterEngine.erl
+++ src/twitterEngine.erl
@@ -8,19 +8,28 @@
 userActor(Username,UserFeed,EnginePID)-> % main userloop
   receive
     {tweet, Message}-> % received from engine
-      userActor(Username,[Message | UserFeed],EnginePID)
+      userActor(Username,[Message | UserFeed],EnginePID);
+    {tweets, ListOfTweets}-> % received from engine as a result of following someone new
+      % should probably sort the new tweets into my feed?
+      userActor(Username,[ListOfTweets|UserFeed],EnginePID)
   end.
 %userActions
 
 userSendTweet(EnginePID,UserName,Tweet)->
   EnginePID ! {distributeTweet,UserName,Tweet}.
 
-userFollowUser(EnginePID,Username,ToFollowUserName)->
-  EnginePID ! {addFollower,Username, ToFollowUserName}.
+userFollowUser(EnginePID,Username,ToFollowUsername)->
+  EnginePID ! {addFollow,Username, ToFollowUsername}.
 
 registerUser(EnginePID, Username)->
   EnginePID ! {registerUser,Username}.
 
+logOn(UserName,EnginePID)->
+  EnginePID ! {logOn,UserName,self()},
+  receive
+    PID->
+      PID %% return the PID of the userActor for the username given
+  end.
 
 %engine stuff
 start()-> % returns enginePID
@@ -30,40 +39,37 @@ start()-> % returns enginePID
 killEngine(EnginePID)->
   EnginePID ! die.
 
-logOn(UserName,EnginePID)->
-  EnginePID ! {logOn,UserName,self()},
-  receive
-    PID->
-      PID %% return the PID of the userActor for the username given
-  end.
-
 engineActor(UserDatabase,HashTagDatabase)->
   receive
     die ->
-      databaseKiller(UserDatabase),
-      databaseKiller(HashTagDatabase); % loop ends here
+      databaseKiller(UserDatabase), % maybe pass in function so that actors are killed? or turn on/off actor?
+      databaseKiller(HashTagDatabase);
+      % loop ends here
     {registerUser,Username}->
       PID = spawn(twitterEngine,userActor,[Username,[],self()]),% actor values = Username,UserFeed,EnginePID
-      insert(UserDatabase,Username, {[],[],[],PID}),%USERDATA = {UsersTweets,Following,Followers,UserActorPID}
+      insert(UserDatabase,Username, {[],[],[],PID}),%USERDATA = {UsersTweets,Following,Followers,UserActorPID} NOTE Following& Followers are those users PIDs
       engineActor(UserDatabase,HashTagDatabase);
     {logOn,Username,PID}->
       {_,_,_,UserActorPID} = query(UserDatabase,Username),
       PID ! UserActorPID,
       engineActor(UserDatabase,HashTagDatabase);
-    {addFollower,Username, ToFollowUserName}->
-      {UsersTweets,Following,Followers,UserActorPID} = query(UserDatabase,Username),
-      insert(UserDatabase,Username,{UsersTweets,[ToFollowUserName| Following],Followers,UserActorPID});
-    {registerHashTag,HashTag,HashTagData}->
-      insert(HashTagDatabase,HashTag,HashTagData),
+    {addFollow,Username, ToFollowUserName}-> % Username now follows ToFollowUsername
+      {A,B,C,FollowUserPID} = query(UserDatabase,ToFollowUserName),
+      {D,E,F,UserActorPID} = query(UserDatabase,Username),
+      insert(UserDatabase,ToFollowUserName,{A,B,[UserActorPID|C],FollowUserPID}),% update followers list
+      %add ToFollowUser's tweetList into users feed (user must organize if desired)
+      UserActorPID ! {tweets,A},
+      insert(UserDatabase,Username,{D,[FollowUserPID| E],F,UserActorPID}), % update following list
       engineActor(UserDatabase,HashTagDatabase);
     {distributeTweet,Tweeter,Tweet}->
       %add parsing to add hashtag tweets to hashTag database
-      {_,_,SendToList,_} = query(UserDatabase,Tweeter),
-      spawn(twitterEngine,distributerActor,[SendToList,Tweet]),
+      {_,_,SendToList,_} = query(UserDatabase,Tweeter), % send to followers
+      spawn(twitterEngine,distributerActor,[SendToList,Tweet]),% send tweet with distributer in own process
       engineActor(UserDatabase,HashTagDatabase);
     %WIP
-    {retriveUser,RequestingProcess,UserName}->
-      RequestingProcess ! query(UserDatabase,UserName);
+    {registerHashTag,HashTag,HashTagData}->
+      insert(HashTagDatabase,HashTag,HashTagData),
+      engineActor(UserDatabase,HashTagDatabase);
     {retriveHashTag,RequestingProcess,HashTag}->
       RequestingProcess ! query(HashTagDatabase,HashTag),
       engineActor(UserDatabase,HashTagDatabase)
